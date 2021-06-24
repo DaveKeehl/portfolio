bd30dededb6b2a5cb28c18a627d60deca16dc199
Davide Ciulla <davide.ciulla@hotmail.com>
2021-06-24 21:52:09 +0200

    Initial styles to container component

--- src/lib/templates/Container.svelte
+++ src/lib/templates/Container.svelte
@@ -1,10 +1,17 @@
-<div class="container">
+<div>
 	<slot />
 </div>
 
-<style>
-	.container {
-		max-width: 80%;
-		background: red;
+<style lang="scss">
+	@import '../../styles/breakpoints.scss';
+
+	div {
+		width: 90%;
+		margin: 0 auto;
+
+		@media (min-width: $sizes-xs) {
+			width: 80%;
+			max-width: 1160px;
+		}
 	}
 </style>
