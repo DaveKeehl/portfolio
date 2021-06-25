0170ad9334949659476c5e1dc5102e0ff72b7c15
Davide Ciulla <davide.ciulla@hotmail.com>
2021-06-25 09:02:46 +0200

    Put actual content in component

--- src/lib/molecules/About.svelte
+++ src/lib/molecules/About.svelte
@@ -1,6 +1,17 @@
-<h2>Section title</h2>
+<script>
+	import Button from '$lib/atoms/Button.svelte';
+</script>
+
+<h2>Let me introduce myself.</h2>
 <p>
-	Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe aut, amet, nihil perspiciatis quam
-	dolore debitis in explicabo corporis sed numquam dolorum eos tempore cumque sit accusantium!
-	Magnam, velit numquam.
+	Hi, my name is Davide Ciulla and I’m a software engineer based in Ticino (southern Switzerland)
+	with a particular interest for front-end web development and UI/UX design.
+	<br />
+	I started playing with web technologies and design in 2014, and I haven’t stopped ever since. Over
+	the years, I constantly improved my skills and managed to learn some incredible technologies that have
+	allowed me to create fresh and modern web applications.
+	<br />
+	In my free time I love playing videogames (Nintendo fanboy right here!), reading comic books, playing
+	the guitar and listening to interesting podcasts about design, tech and space.
 </p>
+<Button />
