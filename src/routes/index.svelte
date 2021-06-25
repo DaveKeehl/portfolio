8aca2c78e2a0f1f4be5dda46abb715a83fe8de24
Davide Ciulla <davide.ciulla@hotmail.com>
2021-06-25 09:03:27 +0200

    Added rest of conditional blocks. Added new layout template component

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,24 +4,29 @@
 	import About from '$lib/molecules/About.svelte';
 	import Container from '$lib/templates/Container.svelte';
 	import SectionNumber from '$lib/atoms/SectionNumber.svelte';
+	import Layout from '$lib/templates/Layout.svelte';
 
 	const ids: number[] = [1, 2, 3, 4];
 </script>
 
-<Head title="Davide Ciulla" />
+<Layout>
+	<Head title="Davide Ciulla" />
 
-<Hero />
+	<Hero />
 
-{#each ids as id}
-	<div class={`${id === 1 && 'gradient-bg'}`}>
-		<Container class="section">
-			<SectionNumber number={id} />
-			<div>
-				<About />
-			</div>
-		</Container>
-	</div>
-{/each}
+	{#each ids as id}
+		<div class={`${id === 1 && 'gradient-bg'}`}>
+			<Container class="section">
+				<SectionNumber number={id} />
+				<div>
+					{#if id === 1}
+						<About />
+					{:else if id === 2}{:else if id === 3}{:else if id === 4}{/if}
+				</div>
+			</Container>
+		</div>
+	{/each}
+</Layout>
 
 <style lang="scss">
 	@import '../styles/colors.scss';
