e3854cd56245c72cffd81e97a5ae1351b695ee77
Davide Ciulla <davide.ciulla@hotmail.com>
2021-07-07 22:01:00 +0200

    Created technologies mapping because of GraphCMS naming

---
+++ src/utils/technologies.ts
@@ -0,0 +1,16 @@
+export const mapping = {
+	GraphCMS: 'GraphCMS',
+	GitHub: 'GitHub',
+	React: 'React',
+	Svelte: 'Svelte',
+	styled_components: 'styled-components',
+	HTML: 'HTML',
+	CSS: 'CSS',
+	JavaScript: 'JavaScript',
+	TypeScript: 'TypeScript',
+	Sass: 'Sass',
+	Storybook: 'Storybook',
+	Jest: 'Jest',
+	Figma_API: 'Figma API',
+	NodeJS: 'NodeJS'
+};
