b0c3fe677e9f326e80c350c247a9f13a109ee8d9
Davide Ciulla <davide.ciulla@hotmail.com>
2021-07-07 22:03:51 +0200

    Added missing elements to header

--- src/lib/organisms/Header.svelte
+++ src/lib/organisms/Header.svelte
@@ -6,36 +6,46 @@
 </script>
 
 <header>
-	<img src={logo.url} alt="" />
+	<a href="https://davideciulla.com">
+		<img src={logo.url} alt="logo" />
+	</a>
 
 	<nav>
 		{#each navigation as item}
-			<a href={`localhost:3000/#${item}`}>{item.toUpperCase()}</a>
+			<a href={`localhost:3000/#${item}`} class="p5--regular">{item.toUpperCase()}</a>
 		{/each}
 	</nav>
 
 	<div>
 		{#each socials as social}
-			<a href={social.url} rel="noopener noreferrer">
-				<img src={social.url} alt={social.name} />
+			<a href={social.url} target="_blank" rel="noopener noreferrer">
+				<img src={social.image.url} alt={social.name} />
 			</a>
 		{/each}
 	</div>
 </header>
 
 <style lang="scss">
+	@import '../../styles/colors.scss';
+
 	header {
 		display: flex;
 		justify-content: space-between;
 		align-items: center;
-		padding: 1rem 0;
+		padding: 3rem 0;
+		color: $white;
 	}
 
-	img {
-		width: 100px;
+	nav a {
+		color: $white;
+
+		&:not(:last-child) {
+			margin-right: 1.5rem;
+		}
 	}
 
-	nav a:not(:last-child) {
-		margin-right: 1.5rem;
+	div {
+		display: flex;
+		gap: 1rem;
 	}
 </style>
