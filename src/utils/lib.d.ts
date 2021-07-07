bd3afd68a42dc422f0445e1aa10bdb761a40bb9b
Davide Ciulla <davide.ciulla@hotmail.com>
2021-07-07 10:06:42 +0200

    Added types and interfaces

--- src/utils/lib.d.ts
+++ src/utils/lib.d.ts
@@ -1,5 +1,85 @@
 export interface IButton {
+	id: string;
 	text: string;
 	link: string;
 	external: boolean;
+	type: string;
+}
+
+export interface ISocial {
+	name: string;
+	url: string;
+	image: {
+		url: string;
+	};
+}
+
+export interface IHeader {
+	logo: {
+		url: string;
+	};
+	navigation: string[];
+	socials: ISocial[];
+}
+
+export interface IHero {
+	greetings: string;
+	valueProposition: string;
+	introduction: string;
+	button: IButton;
+}
+
+export interface IAbout {
+	title: string;
+	text: string;
+	button: IButton;
+}
+
+export interface IProject {
+	id: string;
+	title: string;
+	slug: string;
+	description: string;
+	image: {
+		url: string;
+	};
+	technologies: string[];
+	buttons: IButton[];
+}
+
+export interface IArticle {
+	id: string;
+	title: string;
+	category: string;
+	cover: {
+		url: string;
+	};
+	excerpt: string;
+	content: {
+		html: string;
+	};
+}
+
+export interface IContact {
+	title: string;
+	text: string;
+	email: string;
+}
+
+export interface IFooter {
+	text: {
+		html: string;
+	};
+}
+
+export interface IHomepage {
+	header: IHeader;
+	hero: IHero;
+	about: IAbout;
+	projectSectionTitle: string;
+	projects: IProject[];
+	blogSectionTitle: string;
+	articles: IArticle[];
+	contact: IContact;
+	footer: IFooter;
 }
