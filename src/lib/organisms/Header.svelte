6fd3e1abd3ed29519def55f002b23d3d8e3d0517
Davide Ciulla <davide.ciulla@hotmail.com>
2021-07-09 16:47:06 +0200

    Header now gracefully changes visibility on scroll

--- src/lib/organisms/Header.svelte
+++ src/lib/organisms/Header.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
+	import { fade, fly } from 'svelte/transition';
 	import { dev } from '$app/env';
-	import { getPrefix, website } from '$utils/functions';
+	import { getPrefix } from '$utils/functions';
 	import Container from '$lib/templates/Container.svelte';
 	import type { IHeader } from '$utils/lib';
 
@@ -8,29 +9,59 @@
 	const { logo, navigation, socials } = header;
 
 	const prefix: string = getPrefix(dev);
+
+	let offset = 0;
+	let tolerance = 0;
+
+	let visible: boolean = true;
+	let y = 0;
+	let lastY = 0;
+
+	const checkVisibility = (y: number): boolean => {
+		const dy = lastY - y;
+		lastY = y;
+
+		if (y < offset) return false;
+		if (Math.abs(dy) <= tolerance) return visible;
+		if (dy < 0) return false;
+		return true;
+	};
+
+	$: visible = checkVisibility(y);
+	$: console.log(visible);
 </script>
 
-<header>
+<svelte:window bind:scrollY={y} />
+
+<header class={!visible ? 'hidden' : ''}>
 	<Container>
-		<div class="wrapper">
-			<a href={prefix}>
-				<img src={logo.url} alt={logo.alt} />
-			</a>
+		{#if visible}
+			<div class="wrapper" transition:fly={{ y: -20, duration: 500 }}>
+				<a href={prefix}>
+					<img src={logo.url} alt={logo.alt} />
+				</a>
 
-			<nav>
-				{#each navigation as item}
-					<a href={`${prefix}#${item}`} class="p5--regular">{item.toUpperCase()}</a>
-				{/each}
-			</nav>
+				<nav>
+					{#each navigation as item}
+						<a href={`${prefix}#${item}`} class="p5--medium">{item.toUpperCase()}</a>
+					{/each}
+				</nav>
 
-			<div class="socials">
-				{#each socials as social}
-					<a href={social.url} target="_blank" rel="noopener noreferrer">
-						<img src={social.image.url} alt={social.image.alt} />
-					</a>
-				{/each}
+				<div class="socials">
+					{#each socials as social}
+						<a href={social.url} target="_blank" rel="noopener noreferrer">
+							<img src={social.image.url} alt={social.image.alt} />
+						</a>
+					{/each}
+				</div>
+
+				<img
+					src="/menu.svg"
+					alt="Burger menu icon to toggle mobile menu visibility"
+					class="menu-toggle"
+				/>
 			</div>
-		</div>
+		{/if}
 	</Container>
 </header>
 
@@ -38,11 +69,31 @@
 	@import '../../styles/colors.scss';
 	@import '../../styles/breakpoints.scss';
 
+	header {
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100%;
+		z-index: 10;
+		background: linear-gradient(
+			to bottom,
+			$blue-300,
+			transparentize($blue-300, 0.2) 30%,
+			transparentize($blue-300, 1)
+		);
+		opacity: 1;
+		transition: opacity 0.5s;
+
+		&.hidden {
+			opacity: 0;
+		}
+	}
+
 	.wrapper {
 		display: flex;
 		justify-content: space-between;
 		align-items: center;
-		padding: 3rem 0;
+		padding: 2rem 0 8rem;
 		color: $white;
 	}
 
@@ -57,6 +108,15 @@
 	.socials {
 		display: flex;
 		gap: 1rem;
+	}
+
+	nav,
+	.socials {
+		display: none;
+
+		@media (min-width: $sizes-sm) {
+			display: flex;
+		}
 
 		a {
 			transition: opacity 0.2s;
@@ -67,12 +127,15 @@
 		}
 	}
 
-	nav,
-	.socials {
-		display: none;
+	.menu-toggle {
+		display: block;
 
 		@media (min-width: $sizes-sm) {
-			display: flex;
+			display: none;
+		}
+
+		&:hover {
+			cursor: pointer;
 		}
 	}
 </style>
