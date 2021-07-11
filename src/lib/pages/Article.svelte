356570c9c1a13e2c52b34a36afee2fcd186ca240
Davide Ciulla <davide.ciulla@hotmail.com>
2021-07-11 21:52:27 +0200

    Created article component for blog route

---
+++ src/lib/pages/Article.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import ArticleTop from '$lib/molecules/ArticleTop.svelte';
+	import Container from '$lib/templates/Container.svelte';
+	import SEO from '$lib/templates/SEO.svelte';
+
+	import type { IArticle } from '$utils/lib';
+
+	export let article: IArticle;
+	const { title, category, cover, excerpt, content } = article;
+</script>
+
+<SEO title={`${title} | Davide Ciulla`} description={excerpt} />
+
+<Container>
+	<article>
+		<ArticleTop {title} {category} image={cover} />
+
+		<section>
+			{@html content.html}
+		</section>
+	</article>
+</Container>
+
+<style lang="scss">
+	@import '../../styles/breakpoints.scss';
+
+	article {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+
+	section {
+		width: 100%;
+
+		@media (min-width: $sizes-md) {
+			width: 70%;
+		}
+	}
+</style>
