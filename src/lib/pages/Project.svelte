2cd5388a5911a20bc8db5a0e6d95a20ce914104d
Davide Ciulla <davide.ciulla@hotmail.com>
2021-07-11 17:33:51 +0200

    Coded case study page

---
+++ src/lib/pages/Project.svelte
@@ -0,0 +1,232 @@
+<script lang="ts">
+	import type { IProject } from '$utils/lib';
+	import Container from '$lib/templates/Container.svelte';
+	import ButtonsGroup from '$lib/molecules/ButtonsGroup.svelte';
+	import SEO from '$lib/templates/SEO.svelte';
+
+	export let project: IProject;
+	const {
+		title,
+		liveUrl,
+		repositoryUrl,
+		visitButtonText,
+		image,
+		industry,
+		year,
+		productType,
+		role,
+		description,
+		components,
+		projectSections
+	} = project;
+</script>
+
+<SEO title={`${title} | Davide Ciulla`} {description} />
+
+<Container>
+	<article>
+		<h1 class="h1--bold title">{title}</h1>
+		<h3 class="h3--semibold">Case Study</h3>
+
+		<div class="image">
+			<Container>
+				<img src={image.url} alt={image.alt} />
+			</Container>
+		</div>
+
+		<div class="wrapper">
+			<section>
+				<div class="information">
+					<div>
+						<h4 class="h4--semibold">Industry</h4>
+						<p class="p1--bold">{industry}</p>
+					</div>
+					<div>
+						<h4 class="h4--semibold">Year</h4>
+						<p class="p1--bold">{year}</p>
+					</div>
+					<div>
+						<h4 class="h4--semibold">Product type</h4>
+						<p class="p1--bold">{productType}</p>
+					</div>
+					<div>
+						<h4 class="h4--semibold">Role</h4>
+						<p class="p1--bold">{role.join(', ')}</p>
+					</div>
+				</div>
+				<div class="introduction">
+					<h2 class="h2--bold">In a nutshell.</h2>
+					<p class="p3--regular">{description}</p>
+					<ButtonsGroup
+						primaryButton={{
+							text: visitButtonText,
+							link: liveUrl,
+							external: true
+						}}
+						secondaryButton={{
+							text: 'GitHub Repository',
+							link: repositoryUrl,
+							external: true
+						}}
+						direction="horizontal"
+					/>
+				</div>
+			</section>
+
+			<div class="metrics">
+				<div>
+					<h1 class="h1--bold">{components}</h1>
+					<p class="p4--SEMIBOLD">COMPONENTS</p>
+				</div>
+				<div>
+					<h1 class="h1--bold">{components}</h1>
+					<p class="p4--SEMIBOLD">COMPONENTS</p>
+				</div>
+				<div>
+					<h1 class="h1--bold">{components}</h1>
+					<p class="p4--SEMIBOLD">COMPONENTS</p>
+				</div>
+			</div>
+
+			<div class="content">
+				{#each projectSections as section}
+					<section>
+						<h2 class="h2--semibold">{section.title}</h2>
+						<p class="p3--regular">{@html section.content.html}</p>
+					</section>
+				{/each}
+			</div>
+		</div>
+	</article>
+</Container>
+
+<style lang="scss">
+	@import '../../styles/colors.scss';
+	@import '../../styles/breakpoints.scss';
+
+	article {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+
+	.title {
+		margin-top: 14rem;
+		text-align: center;
+	}
+
+	h3 {
+		color: $blue-100;
+		margin: 1rem 0 4.5rem 0;
+	}
+
+	.image {
+		position: relative;
+		display: flex;
+		justify-content: center;
+		width: 100vw;
+		margin-bottom: 7.5rem;
+
+		@media (min-width: $sizes-md) {
+			margin-bottom: 10rem;
+		}
+
+		&::after {
+			content: '';
+			position: absolute;
+			top: 50%;
+			left: 0;
+			height: 50rem;
+			width: 100%;
+			background: linear-gradient(to bottom, $black, transparentize($black, 1));
+			z-index: -1;
+		}
+
+		img {
+			width: clamp(60%, 500px, 100%);
+			position: relative;
+			left: 50%;
+			transform: translateX(-50%);
+			aspect-ratio: 16 / 10;
+			box-shadow: 0 0 7rem 0.625rem $turquoise-100-a10;
+		}
+	}
+
+	.wrapper {
+		display: flex;
+		flex-direction: column;
+		gap: 7.5rem;
+	}
+
+	section {
+		display: flex;
+		flex-direction: column;
+		gap: 2rem;
+
+		@media (min-width: $sizes-md) {
+			display: grid;
+			grid-template-columns: repeat(12, 1fr);
+		}
+
+		& > *:nth-child(1) {
+			grid-column: 1 / 5;
+			grid-row: 1 / 2;
+		}
+
+		& > *:nth-child(2) {
+			grid-column: 6 / -1;
+			grid-row: 1 / 2;
+		}
+	}
+
+	.information {
+		display: flex;
+		flex-direction: column;
+		gap: 2.5rem;
+		margin-bottom: 6rem;
+
+		@media (min-width: $sizes-md) {
+			margin-bottom: 0;
+		}
+
+		h4 {
+			color: $blue-100;
+		}
+
+		p {
+			color: $white;
+		}
+	}
+
+	.introduction {
+		h2 {
+			margin-bottom: 2rem;
+		}
+
+		p {
+			margin-bottom: 3rem;
+		}
+	}
+
+	.metrics {
+		$border-width: 1px;
+		display: flex;
+		flex-direction: column;
+		gap: 2rem;
+		justify-content: space-around;
+		border-top: $border-width solid $turquoise-100-a10;
+		border-bottom: $border-width solid $turquoise-100-a10;
+		padding: 2rem 0;
+
+		@media (min-width: $sizes-sm) {
+			flex-direction: row;
+			gap: 0;
+		}
+
+		div {
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+		}
+	}
+</style>
---
+++ src/routes/blog/[slug].svelte
@@ -0,0 +1,48 @@
+<script context="module">
+	import { url, getProjectById } from '$utils/queries';
+
+	export async function load({ page, fetch }) {
+		const res = await fetch(url, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				query: getProjectById,
+				variables: {
+					slug: page.params.slug
+				}
+			})
+		});
+
+		if (res.ok) {
+			const { project, header, footer } = (await res.clone().json()).data;
+			return {
+				props: {
+					project,
+					header,
+					footer
+				}
+			};
+		}
+
+		return {
+			status: res.status,
+			error: new Error(`Could not load ${url}`)
+		};
+	}
+</script>
+
+<script lang="ts">
+	import Layout from '$lib/templates/Layout.svelte';
+	import Project from '$lib/pages/Project.svelte';
+	import type { IFooter, IHeader, IProject } from '$utils/lib';
+
+	export let project: IProject;
+	export let header: IHeader;
+	export let footer: IFooter;
+</script>
+
+<Layout {header} {footer}>
+	<Project {project} />
+</Layout>
