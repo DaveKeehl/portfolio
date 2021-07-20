461f598c4b1df531777b20f632490f19117688f4
Davide Ciulla <davide.ciulla@hotmail.com>
2021-07-20 22:49:16 +0200

    Trying to create different transition animations

--- src/actions/reveal.ts
+++ src/actions/reveal.ts
@@ -3,29 +3,78 @@ import { writable } from 'svelte/store';
 interface IOptions {
 	debug?: boolean;
 	ref?: string;
-	duration?: number;
 	threshold?: number;
 	offset?: number;
+	transition?: Transitions;
 	delay?: number;
+	duration?: number;
+	easing?: Easing;
 	x?: number;
 	y?: number;
+	opacity?: number;
 }
 
 interface IReturnAction {
-	destroy(): void;
+	update?: (options?: IOptions) => void;
+	destroy?: () => void;
 }
+
+type Transitions = 'fly' | 'fade' | 'blur' | 'scale' | 'slide';
+type Easing =
+	| 'ease'
+	| 'linear'
+	| 'ease-in'
+	| 'ease-out'
+	| 'ease-in-out'
+	| ['cubic-bezier', number, number, number, number];
 
 const created = writable(false);
 
 const DISABLE_DEBUG = false;
 const DEBUG = false;
 const REF = '';
-const DURATION = 800;
 const THRESHOLD = 0.6;
 const OFFSET = 0;
+const TRANSITION: Transitions = 'fly';
 const DELAY = 0;
-const Y = -20;
+const DURATION = 800;
+const EASING: Easing = 'ease';
 const X = 0;
+const Y = -20;
+const OPACITY = 1;
+
+const getCssProperties = (transition: Transitions, options: IOptions): string => {
+	const { x = X, y = Y } = options;
+
+	console.log(transition);
+
+	if (transition === 'fly') {
+		return `
+			opacity: 0;
+			transform: translateX(${x}px) translateY(${y}px);
+		`;
+	}
+	if (transition === 'fade') {
+		return `
+			opacity: 0;
+		`;
+	}
+	if (transition === 'blur') {
+		return `
+			filter: blur(8px);
+		`;
+	}
+	if (transition === 'scale') {
+		return `
+			transform: scale(0);
+		`;
+	}
+	if (transition === 'slide') {
+		return `
+			transform: transformX(-100%) scale(0);
+		`;
+	}
+};
 
 export const reveal = (node: HTMLElement, options: IOptions = {}): IReturnAction | void => {
 	const {
@@ -33,8 +82,10 @@ export const reveal = (node: HTMLElement, options: IOptions = {}): IReturnAction
 		ref = REF,
 		threshold = THRESHOLD,
 		offset = OFFSET,
-		duration = DURATION,
+		transition = TRANSITION,
 		delay = DELAY,
+		duration = DURATION,
+		easing = EASING,
 		x = X,
 		y = Y
 	} = options;
@@ -48,22 +99,28 @@ export const reveal = (node: HTMLElement, options: IOptions = {}): IReturnAction
 	let createdStyle: boolean;
 	const unsubscribe = created.subscribe((value) => (createdStyle = value));
 
+	let style: HTMLElement;
+
 	if (!createdStyle) {
-		const style = document.createElement('style');
+		style = document.createElement('style');
 		style.setAttribute('type', 'text/css');
-		style.innerHTML = `
-			.not-visible {
-				opacity: 0;
-				transform: translateX(${x}px) translateY(${y}px);
-			}
-		`;
+		style.setAttribute('data-action', 'reveal');
 		const head = document.querySelector('head');
 		head.appendChild(style);
 		created.set(true);
 	}
 
-	node.classList.add('not-visible');
-	node.style.transition = `all ${duration / 1000}s ${delay / 1000}s`;
+	if (style && createdStyle) {
+		const prev = style.innerHTML;
+		style.innerHTML = prev.concat(`		
+			.${transition}--hidden {
+				${getCssProperties(transition, options)}
+			}
+		`);
+	}
+
+	node.classList.add(`${transition}--hidden`);
+	node.style.transition = `all ${duration / 1000}s ${delay / 1000}s ${easing}`;
 
 	const observer = new IntersectionObserver(
 		(entries: IntersectionObserverEntry[], observer: IntersectionObserver) => {
@@ -81,7 +138,7 @@ export const reveal = (node: HTMLElement, options: IOptions = {}): IReturnAction
 
 			entries.forEach((entry) => {
 				if (entry.intersectionRatio >= threshold) {
-					node.classList.remove('not-visible');
+					node.classList.remove(`${transition}--hidden`);
 					observer.unobserve(node);
 
 					return {
