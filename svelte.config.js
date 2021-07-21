674865b7f72bc6bbc20e8cb6964cc76bee87163a
Davide Ciulla <davide.ciulla@hotmail.com>
2021-07-21 18:17:26 +0200

    Installed and set adapter-static

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import preprocess from 'svelte-preprocess';
 import path from 'path';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 
@@ -9,8 +10,7 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
+		target: '#svelte', // hydrate the <div id="svelte"> element in src/app.html
 		vite: {
 			resolve: {
 				alias: {
@@ -24,7 +24,13 @@ const config = {
 					$pages: path.resolve('src/lib/pages')
 				}
 			}
-		}
+		},
+		adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		})
 	}
 };
 
