edf8db8a8a49c28ad5106bcc8ec11f2cfd64e4f2
Davide Ciulla <davide.ciulla@hotmail.com>
2021-07-26 18:35:15 +0200

    SEO component now accepts an optional image

--- src/lib/templates/SEO.svelte
+++ src/lib/templates/SEO.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	export let title: string;
 	export let description: string;
+	export let image: string = '/og_image.png';
 </script>
 
 <svelte:head>
@@ -10,7 +11,7 @@
 	<meta property="og:description" content={description} />
 	<meta property="og:type" content="website" />
 	<meta property="og:url" content="https://davideciulla.com" />
-	<meta property="og:image" content="/og_image.png" />
+	<meta property="og:image" content={image} />
 	<meta property="twitter:card" content="summary_card_large" />
 	<meta property="twitter:creator" content="Davide Ciulla" />
 	<meta property="twitter:description" content={description} />
