bdd00113cadf766c1318faa84eeb430d72c62ea3
Davide Ciulla <davide.ciulla@hotmail.com>
2022-11-22 00:20:03 +0100

    Went back to using SSR in prod

--- svelte.config.js
+++ svelte.config.js
@@ -1,38 +1,28 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 import path from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: preprocess(),
 
-	kit: {
-		adapter: adapter({
-			// default options are shown
-			pages: 'build',
-			assets: 'build',
-			fallback: null,
-			precompress: false,
-			strict: true
-		}),
-		alias: {
-			$components: path.resolve('./src/lib/components'),
-			$utils: path.resolve('./src/utils'),
-			$pages: path.resolve('./src/lib/pages')
-		},
-		prerender: {
-			entries: [
-				'/',
-				'/article/welcome-to-my-portfolio',
-				'/project/stack-and-heap-diagram',
-				'/project/fonts-jar',
-				'/project/f1-insights',
-				'/project/svelte-reveal'
-			]
-		}
-	}
+  kit: {
+    adapter: adapter({
+      // default options are shown
+      pages: 'build',
+      assets: 'build',
+      fallback: null,
+      precompress: false,
+      strict: true
+    }),
+    alias: {
+      $components: path.resolve('./src/lib/components'),
+      $utils: path.resolve('./src/utils'),
+      $pages: path.resolve('./src/lib/pages')
+    }
+  }
 };
 
 export default config;
