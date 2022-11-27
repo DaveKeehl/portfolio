1b1d038bac0ca5cfa6d6eaf89008584f01a75b3a
Davide Ciulla <davide.ciulla@hotmail.com>
2022-11-27 22:55:49 +0100

    Ported to TW

--- src/lib/components/ButtonsGroup.svelte
+++ src/lib/components/ButtonsGroup.svelte
@@ -1,41 +1,31 @@
 <script lang="ts">
-	import Button from '$lib/components/Button.svelte';
+  import Button from '$lib/components/Button.svelte';
 
-	import type { IButton } from '$utils/lib';
-	import { css } from '$utils/stitches.config';
+  import type { IButton } from '$utils/lib';
+  import { css } from '$utils/stitches.config';
 
-	export let primary: IButton;
-	export let secondary: IButton;
-	export let size: 'small' | 'large';
-
-	const groupStyles = css({
-		display: 'grid',
-		gridTemplateColumns: '1fr',
-		gap: '1rem',
-
-		'@md': {
-			display: 'flex'
-		}
-	});
+  export let primary: IButton;
+  export let secondary: IButton;
+  export let size: 'small' | 'large';
 </script>
 
 <div class="button-group">
-	<div class={groupStyles()}>
-		<Button
-			variant="primary"
-			{size}
-			href={primary.href}
-			external={primary.external}
-		>
-			{primary.text}
-		</Button>
-		<Button
-			variant="secondary"
-			{size}
-			href={secondary.href}
-			external={secondary.external}
-		>
-			{secondary.text}
-		</Button>
-	</div>
+  <div class="grid grid-cols-1 gap-4 md:flex">
+    <Button
+      variant="primary"
+      {size}
+      href={primary.href}
+      external={primary.external}
+    >
+      {primary.text}
+    </Button>
+    <Button
+      variant="secondary"
+      {size}
+      href={secondary.href}
+      external={secondary.external}
+    >
+      {secondary.text}
+    </Button>
+  </div>
 </div>
