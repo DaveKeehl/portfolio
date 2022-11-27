734a300e00f64a22e8b03509e17a59134dddb607
Davide Ciulla <davide.ciulla@hotmail.com>
2022-11-27 21:21:46 +0100

    Using Tailwind

--- src/lib/components/Socials.svelte
+++ src/lib/components/Socials.svelte
@@ -1,35 +1,19 @@
 <script lang="ts">
-	import SanityImage from './SanityImage.svelte';
+  import SanityImage from './SanityImage.svelte';
+  import type { ISocial } from '$utils/lib';
 
-	import type { ISocial } from '$utils/lib';
-	import { css } from '$utils/stitches.config';
-
-	export let socials: ISocial[];
-
-	const socialsStyles = css({
-		display: 'flex',
-		gap: '0.75rem'
-	});
-
-	const socialStyles = css({
-		display: 'flex',
-		transition: 'opacity 0.2s',
-
-		'&:hover': {
-			opacity: '0.8'
-		}
-	});
+  export let socials: ISocial[];
 </script>
 
-<div class={socialsStyles()}>
-	{#each socials as social}
-		<a
-			href={social.url}
-			target="_blank"
-			rel="noopener noreferrer"
-			class={socialStyles()}
-		>
-			<SanityImage src={social.icon.assetId} alt={social.title} />
-		</a>
-	{/each}
+<div class="flex gap-3">
+  {#each socials as social}
+    <a
+      href={social.url}
+      target="_blank"
+      rel="noopener noreferrer"
+      class="flex hover:opacity-80 transition-opacity"
+    >
+      <SanityImage src={social.icon.assetId} alt={social.title} />
+    </a>
+  {/each}
 </div>
