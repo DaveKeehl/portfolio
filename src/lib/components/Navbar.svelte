1edb7ea5c146a6ede6c610bcb707c0ec116110e9
Davide Ciulla <davide.ciulla@hotmail.com>
2022-11-27 23:06:09 +0100

    Ported to TW

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,48 +1,23 @@
 <script lang="ts">
-	import { getHostname } from '$utils/functions';
-	import type { ISection } from '$utils/lib';
-	import { css } from '$utils/stitches.config';
-	import { section } from '$utils/stores';
+  import { getHostname } from '$utils/functions';
+  import type { ISection } from '$utils/lib';
+  import { section } from '$utils/stores';
 
-	export let sections: ISection[];
+  export let sections: ISection[];
 
-	const prefix: string = getHostname();
-	const getHref = (sectionName: string) => `${prefix}#${sectionName}`;
+  const prefix: string = getHostname();
+  const getHref = (sectionName: string) => `${prefix}#${sectionName}`;
 
-	const navStyles = css({
-		display: 'flex',
-		gap: '1.5rem'
-	});
-
-	const navItemStyles = css({
-		display: 'flex',
-		color: '$blue-200',
-		fontSize: '$p5',
-		fontWeight: '600',
-		lineHeight: '24px',
-		letterSpacing: '2%',
-		textTransform: 'uppercase',
-		transition: 'color 0.2s',
-
-		'&:hover': {
-			color: '$grayscale-100'
-		},
-
-		'&.active': {
-			color: '$grayscale-100'
-		}
-	});
-
-	$: currentSection = $section;
+  $: currentSection = $section;
 </script>
 
-<nav class={navStyles()}>
-	{#each sections as section}
-		<a
-			href={getHref(section.name.toLowerCase())}
-			class={navItemStyles()}
-			class:active={currentSection === section.name.toLowerCase()}
-			>{section.name}</a
-		>
-	{/each}
+<nav class="flex gap-6">
+  {#each sections as section}
+    <a
+      href={getHref(section.name.toLowerCase())}
+      class="flex text-blue-200 text-p5 font-semibold leading-6 tracking-[2%] uppercase transition-colors hover:text-grayscale-100"
+      class:text-grayscale-100={currentSection === section.name.toLowerCase()}
+      >{section.name}</a
+    >
+  {/each}
 </nav>
