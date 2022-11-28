235a10da2521daca4c48e57aad7062b11903e052
Davide Ciulla <davide.ciulla@hotmail.com>
2022-11-28 12:51:22 +0100

    Ported to TW

--- src/lib/components/RichContent.svelte
+++ src/lib/components/RichContent.svelte
@@ -1,60 +1,76 @@
 <script lang="ts">
-	import { PortableText } from '@portabletext/svelte';
-	import { css } from '$utils/stitches.config';
+  import { PortableText } from '@portabletext/svelte';
 
-	let className = '';
+  let className = '';
 
-	export let content: any;
-	export { className as class };
-
-	const richContentStyles = css({
-		color: '$blue-100',
-		textAlign: 'left',
-
-		'& > *:not(:last-child)': {
-			marginBottom: '24px'
-		},
-
-		'&:first-child': {
-			marginTop: '0'
-		},
-
-		'& p': {
-			p4: 'regular',
-			opacity: '0.9'
-		},
-
-		'& a': {
-			color: '$turquoise-200',
-
-			'&:hover': {
-				textDecoration: 'underline'
-			}
-		},
-
-		'& h1, h2, h3, h4': {
-			color: '$grayscale-100'
-		},
-
-		'& h1:not(:first-child), h2:not(:first-child), h3:not(:first-child), h4:not(:first-child)':
-			{
-				marginBlock: '64px 32px'
-			},
-
-		'& h1': {
-			h1: 'bold'
-		},
-
-		'& h2': {
-			h2: 'bold'
-		},
-
-		'& h3': {
-			h3: 'bold'
-		}
-	});
+  export let content: any;
+  export { className as class };
 </script>
 
-<div class={`${richContentStyles()} ${className}`}>
-	<PortableText value={content} />
+<div class={`rich-text text-blue-100 text-left ${className}`}>
+  <PortableText value={content} />
 </div>
+
+<style lang="scss" global>
+  .rich-text {
+    & > *:not(:last-child) {
+      margin-bottom: 24px;
+    }
+
+    &:first-child {
+      margin-top: 0;
+    }
+
+    & p {
+      font-family: theme('fontFamily.body');
+      font-size: theme('fontSize.p4');
+      font-weight: theme('fontWeight.normal');
+      line-height: 150%;
+      letter-spacing: 0.02em;
+      opacity: 0.9;
+    }
+
+    & a {
+      color: theme('colors.turquoise.200');
+
+      &:hover {
+        text-decoration: underline;
+      }
+    }
+
+    & h1,
+    h2,
+    h3,
+    h4 {
+      color: theme('colors.grayscale.100');
+    }
+
+    & h1:not(:first-child),
+    h2:not(:first-child),
+    h3:not(:first-child),
+    h4:not(:first-child) {
+      margin-block: 64px 32px;
+    }
+
+    & h1 {
+      font-family: theme('fontFamily.heading');
+      font-size: theme('fontSize.h1');
+      font-weight: theme('fontWeight.bold');
+      line-height: 120%;
+    }
+
+    & h2 {
+      font-family: theme('fontFamily.heading');
+      font-size: theme('fontSize.h2');
+      font-weight: theme('fontWeight.bold');
+      line-height: 140%;
+    }
+
+    & h3 {
+      font-family: theme('fontFamily.heading');
+      font-size: theme('fontSize.h3');
+      font-weight: theme('fontWeight.bold');
+      line-height: 150%;
+    }
+  }
+</style>
