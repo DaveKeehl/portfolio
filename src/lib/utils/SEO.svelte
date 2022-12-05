177bd4da11217e76daa7959bb38e83cba976ebbe
Davide Ciulla <davide.ciulla@hotmail.com>
2022-12-05 21:40:46 +0100

    Moved some metatags to the html base file

--- src/lib/utils/SEO.svelte
+++ src/lib/utils/SEO.svelte
@@ -8,12 +8,6 @@
 </script>
 
 <svelte:head>
-  <meta charset="utf-8" />
-  <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-  <meta
-    name="viewport"
-    content="width=device-width, initial-scale=1, shrink-to-fit=no"
-  />
   <title>{title}</title>
   <meta name="description" content={description} />
   <meta property="og:title" content={title} />
