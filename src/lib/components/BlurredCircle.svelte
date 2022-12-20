238732c48a4a6015566d1ff6c890873816558d86
Davide Ciulla <davide.ciulla@hotmail.com>
2022-12-20 14:43:15 +0100

    Better compatibility on safari and firefox

--- src/lib/components/BlurredCircle.svelte
+++ src/lib/components/BlurredCircle.svelte
@@ -6,3 +6,23 @@
 <div
   class={`absolute bottom-0 left-1/2 -z-[1] -translate-x-1/2 translate-y-1/2 w-[600px] aspect-square blur-circle opacity-25 rounded-full bg-blue-100 ${className}`}
 />
+
+<style>
+  div {
+    backface-visibility: hidden;
+    -webkit-backface-visibility: hidden;
+    -moz-backface-visibility: hidden;
+    transform: translate3d(-50%, 50%, 0);
+    -webkit-transform: translate3d(-50%, 50%, 0);
+    -moz-transform: translate3d(-50%, 50%, 0);
+  }
+
+  @supports (-moz-appearance: none) {
+    div {
+      opacity: 0.1;
+      transform: translate3d(-50%, 50%, 0) scale(1.5);
+      -webkit-transform: translate3d(-50%, 50%, 0) scale(1.5);
+      -moz-transform: translate3d(-50%, 50%, 0) scale(1.5);
+    }
+  }
+</style>
