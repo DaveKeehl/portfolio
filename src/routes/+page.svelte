e611f8baba12ba3d08d3cd04ea9f8794ab1d9e68
Davide Ciulla <davide.ciulla@hotmail.com>
2022-12-22 12:52:22 +0100

    Formatted with spaces

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,20 @@
 <script lang="ts">
-	import { afterUpdate, onMount } from 'svelte';
-	import Home from '$lib/pages/Home.svelte';
-	import type { PageData } from './$types';
-	import { section } from '$utils/stores';
+  import { afterUpdate, onMount } from 'svelte';
+  import Home from '$lib/pages/Home.svelte';
+  import type { PageData } from './$types';
+  import { section } from '$utils/stores';
 
-	export let data: PageData;
-	let { siteSettings, homepage, header, footer } = data;
-	let show = false;
+  export let data: PageData;
+  let { siteSettings, homepage, header, footer } = data;
+  let show = false;
 
-	onMount(() => section.set(''));
+  onMount(() => section.set(''));
 
-	afterUpdate(() => {
-		show = true;
-	});
+  afterUpdate(() => {
+    show = true;
+  });
 </script>
 
 {#if show}
-	<Home {siteSettings} {homepage} {header} {footer} />
+  <Home {siteSettings} {homepage} {header} {footer} />
 {/if}
